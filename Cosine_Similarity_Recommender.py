4137e95bc42fe6f634e62337c258009da482684d
Yashwant Parmar <yashwant.parmar@gmail.com>
2022-10-13 09:07:47 -0700

    Added some comments for readability

--- Cosine_Similarity_Recommender.py
+++ Cosine_Similarity_Recommender.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.metrics.pairwise import linear_kernel
 import numpy as np
 
-df = pd.read_csv('data/audioset_dataframe.csv', converters={'audio': pd.eval})
+df = pd.read_csv('data/audioset_dataframe.csv', converters={'audio': pd.eval}, nrows=5)
 
 def recommendations(dataframe, video_id):
     df_rec = dataframe
@@ -12,14 +12,17 @@ def recommendations(dataframe, video_id):
 
     # because the data has already been normalized, cosine similarity can be calculated by simply using a dot product
     cos_sim = pd.DataFrame(linear_kernel(aud_embeds, aud_embeds))
+    print(cos_sim)
 
     # matches the indices of the cos_sim array to the video ids
     indices = pd.Series(aud_embeds.index, index=df_rec['video_id']).drop_duplicates()
 
     # finds indices of video id
     idx = indices[video_id]
+    print(idx)
     # finds the highest cosine similarities
     similarity_scores = list(enumerate(cos_sim[idx]))
+    print(similarity_scores)
     similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
     top_five = similarity_scores[1:6]
     video_indices = [i[0] for i in top_five]
--- New_Sounds_Recommender.py
+++ New_Sounds_Recommender.py
@@ -9,20 +9,23 @@ def norm(x):
 
 def AudioRecommender(path_to_new_sounds):
     """A function that takes a path to the audioset_dataframe.csv and where new sounds recommendations are wanted for
-    and returns video recommendations. Comments are minimal as the code is largely reused from other files.
+    and returns video recommendations.
     """
-    df_trained = pd.read_csv('data/audioset_dataframe.csv', converters={'audio': pd.eval}, nrows=15)
+
+    df_trained = pd.read_csv('data/audioset_dataframe.csv', converters={'audio': pd.eval})
 
     # formats new audio input correctly
     data_init = []
     for file in os.listdir(path_to_new_sounds):
-        if file.endswith('.tfrecord'): # filters out the audio files themselves, bringing in only the embeddings
+        # filters out the audio files themselves, bringing in only the embeddings. Could be avoided with the use of folders.
+        if file.endswith('.tfrecord'):
             data_init.append(f'{path_to_new_sounds}{file}')
 
     data_tf = tf.data.TFRecordDataset(data_init)
     features = []
     cutoff = 10
     bits = 128
+    # for providing generic, easy-to-find names for the new videos
     new_vid_id = 1
 
     for tf_file in data_tf:
@@ -40,6 +43,7 @@ def AudioRecommender(path_to_new_sounds):
         features.append(feats)
         new_vid_id += 1
 
+    # puts the new audio into the same dataframe as the ones downloaded from AudioSet
     df_new = pd.DataFrame.from_records(features)
     df_new['audio'] = df_new['audio'].apply(norm)
     df_total = pd.concat([df_trained, df_new]).reset_index()
@@ -47,15 +51,18 @@ def AudioRecommender(path_to_new_sounds):
     aud_embeds = pd.DataFrame(df_total['audio'].tolist())
     aud_embeds = aud_embeds.fillna(0)
 
+    # because the new audio is at the end, we can iterate backwards to analyze it all
     i = -1
     for new_audio in df_new:
 
         cos_sim = pd.DataFrame(linear_kernel(np.array(aud_embeds), np.array(aud_embeds)[i].reshape(1, -1)), columns=['cos_sim'])
 
+        # creates a temporary dataframe that contains both similarity scores and video links
         temp = df_total.join(cos_sim)
+        # removes the new audio from the dataset, as we don't want to find similar audio files but instead videos
         temp = temp[~temp['video_id'].str.contains('new_video')]
+        # finds the five most similar videos
         temp = temp.sort_values(by=['cos_sim'], ascending=False)
-
         top_five = temp['video_id'].iloc[1:6]
         top_five = 'youtube.com/watch?v=' + top_five.astype(str)
         print(data_init[i])
